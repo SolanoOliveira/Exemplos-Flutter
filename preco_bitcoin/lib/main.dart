11217f1c7539fecb59ffd236e08c6872d82cf705
Solano Lima OLiveira <solano.oliveira@icomp.ufam.edu.br>
2020-04-20 23:06:31 -0400

    Add files via upload

---
+++ preco_bitcoin/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:precobitcoin/home.dart';
+
+void main() => runApp(
+
+    MaterialApp(
+      home: home() ,
+      debugShowCheckedModeBanner: false,
+
+
+    )
+
+
+);
+
